205318c13f84695ce918c692f95e8bcf6a0f82b5
Matt Lavoie <mattplavoie@gmail.com>
2016-04-28 00:40:03 -0400

    Getting google sheets api working with Tabletop
    Proof of concept

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 /* global angular */
 /* global _ */
+
 'use strict';
 
 console.log("Hi. There isn't too much to find in here, but feel free to dig around the public GitHub repo https://github.com/MattPLavoie/mattplavoie.github.io");
 
-var apiUrl = "";
+var api = {
+  events: "1JNlc1HvUc0a2p3Yk2ilvk8c5RLcvmhc-1RCOvHJ1WJU",
+  references: ""
+ };
+
 
 var fetch = function($http, requestUrl, success) {
   $http.get(requestUrl)
@@ -15,7 +20,7 @@ var fetch = function($http, requestUrl, success) {
     });
   };
 
-  angular.module('myApp', [])
+  angular.module('myApp', ['ngSanitize'])
   .controller('appController', ['$scope', '$http', appController]);
 
 
@@ -28,5 +33,18 @@ var fetch = function($http, requestUrl, success) {
       $scope.showSpeakingPast = mode == 'past';
     }
 
-
+    Tabletop.init( {
+      key: api.events,
+      callback: function(data, tabletop) {
+        $scope.events = _.forEach(data, function(row) {
+          row = _.update(row, 'date', function(date) { return moment(date,"MM-DD-YYYY").format("dddd, MMMM Do YYYY"); });
+          row = _.update(row, 'description', function(description) { return description.split(/\n/g); })
+          return row;
+        });
+        $scope.$digest();
+      },
+      simpleSheet: true,
+      orderby: 'date',
+      reverse: true
+    } );
   }
