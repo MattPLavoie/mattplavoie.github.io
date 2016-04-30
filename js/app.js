6038453859ea0256d7f4c323cc9eb2736761b023
Matt Lavoie <mattplavoie@gmail.com>
2016-04-29 22:21:17 -0400

    got both events and references working

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ console.log("Hi. There isn't too much to find in here, but feel free to dig arou
 
 var api = {
   events: "1JNlc1HvUc0a2p3Yk2ilvk8c5RLcvmhc-1RCOvHJ1WJU",
-  references: ""
+  references: "1X41NyFxfKDsdjgHbKWQ2XdL0Cf4vDQmxkOnRh4JvQNQ"
  };
 
 
@@ -33,15 +33,42 @@ var fetch = function($http, requestUrl, success) {
       $scope.showSpeakingPast = mode == 'past';
     }
 
+    var eventsTransform = function (data) {
+      $scope.events = _.forEach(data, function(row) {
+
+        row = _.update(row, 'date', function(date) {
+          var m = moment(date,"MM-DD-YYYY");
+          row.displayDate = m.format("dddd, MMMM Do YYYY");
+          return m;
+        });
+
+        row = _.update(row, 'description', function(description) {
+          return description.split(/\n/g);
+        })
+
+        return row;
+      });
+    }
+
     Tabletop.init( {
       key: api.events,
       callback: function(data, tabletop) {
-        $scope.events = _.forEach(data, function(row) {
-          row = _.update(row, 'date', function(date) { return moment(date,"MM-DD-YYYY").format("dddd, MMMM Do YYYY"); });
-          row = _.update(row, 'description', function(description) { return description.split(/\n/g); })
-          return row;
-        });
-        $scope.$digest();
+
+        eventsTransform(data);
+
+        Tabletop.init( {
+          key: api.references,
+          callback: function(data, tabletop) {
+
+            $scope.references = data;
+
+            $scope.$digest();
+          },
+          simpleSheet: true,
+          orderby: 'date',
+          reverse: true
+        } );
+
       },
       simpleSheet: true,
       orderby: 'date',
