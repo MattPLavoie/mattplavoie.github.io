a29bc9f5a2cee75372ba8f55328acf5bac0d5c06
Matt Lavoie <mattplavoie@gmail.com>
2017-05-14 11:43:47 -0400

    Major refactor, cleanup, and about placeholder

---
+++ js/controllers.js
@@ -0,0 +1,24 @@
+app.controller('resourcesController', ['$scope', '$http', '$routeParams', resourcesController]);
+app.controller('eventsController', ['$scope', '$http', eventsController]);
+app.controller('aboutController', ['$scope', '$http', aboutController]);
+
+
+function resourcesController($scope, $http, $routeParams) {
+    $scope.$parent.view = "resources";
+
+    if ($routeParams.resourceId) {
+        $scope.resourceId = $routeParams.resourceId;
+        $scope.$parent.view = "present";
+    } else {
+        $scope.$parent.view = "resources";
+    }
+}
+
+function eventsController($scope, $http) {
+  $scope.$parent.view = "events";
+}
+
+function aboutController($scope, $http) {
+  $scope.$parent.view = "about";
+  console.log('ab')
+}
--- js/present.js
+++
@@ -1,8 +0,0 @@
-app.controller('presentationController', ['$scope', '$http', '$routeParams', resourcesController]);
-
-function presentationController($scope, $http, $routeParams) {
-    console.log('hello');
-  $scope.$parent.view = "present";
-  $scope.resourceId = $routeParams.resourceId;
-  console.log('a',$routeParams.resourceId);
-}
